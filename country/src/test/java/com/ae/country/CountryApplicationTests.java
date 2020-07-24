efb3e3f03760fda067ee0440d016740f8ef6d30a
Andres <andres.rivas@mercadolibre.com>
2020-07-24 08:54:57 -0300

    Initial commit

---
+++ country/src/test/java/com/ae/country/CountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
