14d3fa10bbff0041af0655cc2815f711a998390a
Andres <andres.rivas@mercadolibre.com>
2020-07-27 19:20:33 -0300

    Added CRUD methods

--- league-service/src/main/java/com/ae/league/city/service/CityService.java
+++ league-service/src/main/java/com/ae/league/city/service/CityService.java
@@ -10,4 +10,27 @@ public interface CityService {
    *  @return list of cities
    *  */
   public List<City> getAllCities();
+
+  /**
+   * @param id of city to get
+   * @return city founded
+   */
+  public City getCityById(Long id);
+
+  /**
+   * @param city to make creation
+   * @return city created
+   */
+  public City createCity(City city);
+
+  /**
+   * @param city to update
+   * @return city updated
+   */
+  public City updateCity(City city);
+
+  /**
+   *  @param id of city to delete
+   *  */
+  public void deleteCity(Long id);
 }
