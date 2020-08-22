c950be515c4abf93b0dc1c45d4c17a22683813b9
Andres <andres.rivas@mercadolibre.com>
2020-08-22 12:39:02 -0300

    Refactor coding

--- league-service/src/main/java/com/ae/league/util/CityDtoConverter.java
+++ league-service/src/main/java/com/ae/league/util/CityDtoConverter.java
@@ -19,21 +19,21 @@ public class CityDtoConverter {
         this.modelMapper = modelMapper;
     }
 
-  /**
-   * Converter city entity to city response
-   * @param cityEntity to converter
-   * @return city response
-   */
-  public CityResponse converterCityToDto(CityEntity cityEntity) {
+    /**
+    * Converter city entity to city response
+    * @param cityEntity to converter
+    * @return city response
+    */
+    public CityResponse converterCityToDto(CityEntity cityEntity) {
         return modelMapper.map(cityEntity, CityResponse.class);
     }
 
-  /**
-   * Converter list of city entity to city response
-   * @param cityEntities list
-   * @return list of city response
-   */
-  public List<CityResponse> converterCityToDto(List<CityEntity> cityEntities) {
+    /**
+    * Converter list of city entity to city response
+    * @param cityEntities list
+    * @return list of city response
+    */
+    public List<CityResponse> converterCityToDto(List<CityEntity> cityEntities) {
         return cityEntities.stream()
                 .map(this::converterCityToDto)
                 .collect(Collectors.toList());
