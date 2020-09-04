a120e847e9e878fc3e870c1b7f40064e3aa99193
Andres <andres.rivas@mercadolibre.com>
2020-09-04 08:17:41 -0300

    Dto to request added

--- league-service/src/main/java/com/ae/league/dto/CountryRequest.java
+++ league-service/src/main/java/com/ae/league/dto/CountryRequest.java
@@ -1,6 +1,8 @@
 package com.ae.league.dto;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.sun.istack.NotNull;
+import com.sun.istack.Nullable;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +11,12 @@ import java.util.List;
 @Data
 @Builder
 public class CountryRequest {
+    @Nullable
     private Long id;
+    @NotNull
     private String code;
+    @NotNull
     private String name;
+    @Nullable
     private List<CityRequest> cities;
 }
