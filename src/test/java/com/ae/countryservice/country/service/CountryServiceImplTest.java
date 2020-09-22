53771623e2876390a7ce7a024cc185e1618a9101
Andres <andres.rivas@mercadolibre.com>
2020-09-22 08:36:34 -0300

    Get all countries test added

--- src/test/java/com/ae/countryservice/country/service/CountryServiceImplTest.java
+++ src/test/java/com/ae/countryservice/country/service/CountryServiceImplTest.java
@@ -1,8 +1,15 @@
 package com.ae.countryservice.country.service;
 
+import com.ae.countryservice.country.repository.CountryRepository;
+import com.ae.countryservice.util.CountryMockUtil;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,11 +17,25 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 class CountryServiceImplTest {
 
+    @InjectMocks
+    CountryServiceImpl countryService;
+
+    @Mock
+    CountryRepository repository;
 
 
     @BeforeEach
     void setUp() {
     }
+
+    @Test
+    public void shouldGetCountriesListWhenIsNotNull() {
+        Mockito.when(repository.findAll()).thenReturn(CountryMockUtil.getAllCountries());
+
+        Assertions.assertEquals(CountryMockUtil.getAllCountries(), countryService.getAllCountries());
+
+    }
+
 
     @AfterEach
     void tearDown() {
