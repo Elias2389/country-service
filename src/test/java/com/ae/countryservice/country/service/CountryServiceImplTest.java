fbd4b42f335ac09acdfc14124d659b663df8c586
Andres <andres.rivas@mercadolibre.com>
2020-09-22 07:40:04 -0300

    Added initial service test

---
+++ src/test/java/com/ae/countryservice/country/service/CountryServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.ae.countryservice.country.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
