501374735d019b0f2b0baecf9477d288973cb573
Andres <andres.rivas@mercadolibre.com>
2020-09-22 08:09:17 -0300

    Added utils

--- src/main/java/com/ae/countryservice/CountryServiceApplication.java
+++ src/main/java/com/ae/countryservice/CountryServiceApplication.java
@@ -2,10 +2,9 @@ package com.ae.countryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class CountryServiceApplication {
 
--- src/test/java/com/ae/countryservice/country/service/CountryServiceImplTest.java
+++ src/test/java/com/ae/countryservice/country/service/CountryServiceImplTest.java
@@ -2,10 +2,15 @@ package com.ae.countryservice.country.service;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
 class CountryServiceImplTest {
+
+
 
     @BeforeEach
     void setUp() {
