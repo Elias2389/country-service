151f83f8db6ac75bb317b9d6e38c0f8e6ad85796
Andres <andres.rivas@mercadolibre.com>
2020-09-24 08:24:53 -0300

    Added changes

---
+++ src/test/java/com/ae/countryservice/country/controller/CountryControllerTest.java
@@ -0,0 +1,15 @@
+package com.ae.countryservice.country.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class CountryControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
