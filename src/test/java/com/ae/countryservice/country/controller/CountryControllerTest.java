8692b1e9da53d67fef3b0371e8794d83662dd119
Andres <andres.rivas@mercadolibre.com>
2020-09-24 08:26:23 -0300

    Added port to test

--- src/test/java/com/ae/countryservice/country/controller/CountryControllerTest.java
+++ src/test/java/com/ae/countryservice/country/controller/CountryControllerTest.java
@@ -2,11 +2,13 @@ package com.ae.countryservice.country.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CountryControllerTest {
 
     @BeforeEach
